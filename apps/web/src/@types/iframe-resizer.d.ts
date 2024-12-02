1528a7b97ecfe0be36bebdf57ede90c63b55d9b9
jrbertt <joaorebertt0219@gmail.com>
2024-12-02 08:49:33 -0300

    fix: eslint error

--- apps/web/src/@types/iframe-resizer.d.ts
+++ apps/web/src/@types/iframe-resizer.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'iframe-resizer/js/iframeResizer' {
   const iframeResizer: (options: any, selector: string | HTMLElement) => void
   export = iframeResizer
